22214865d6aa4bc2803bb9d52dd36a40134a15e0
civanahmetyasin <civan.ahmetyasin@gmail.com>
2020-03-22 13:16:04 +0300

    ekrani tekrar kapatmadan yeni sayfa acarak bir sonraki robota geciyor

--- stokbilgisiGetir.py
+++ stokbilgisiGetir.py
@@ -15,11 +15,6 @@ mevcutStok = driver.find_element_by_id('myPopup3').text
 
 print(mevcutStok.split(': ')[1])
 
-driver.close()
-
-driver = webdriver.Chrome('C:/KomutSatiriProg/chromedriver.exe')  # Optional argument, if not specified will search path.
-driver.set_window_position(0,0)
-driver.set_window_size(800,800)
 driver.get('https://www.robotistan.com/makeblock-mbot-ranger-bluetooth')
 time.sleep(5)
 driver.find_element_by_name('Adet5798').send_keys('100000')
