6cd568591bba682352fc5d2ed204c9f246dd1375
civanahmetyasin <civan.ahmetyasin@gmail.com>
2020-03-22 13:15:17 +0300

    Stok durumlari ogrenip ekrana yazıyor.

---
+++ stokbilgisiGetir.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome('C:/KomutSatiriProg/chromedriver.exe')  # Optional argument, if not specified will search path.
+driver.set_window_position(0,0)
+driver.set_window_size(800,800)
+driver.get('https://www.robotistan.com/makeblock-mbot-bluetooth-kiti-v11-mavi')
+time.sleep(5)
+driver.find_element_by_name('Adet2754').send_keys('100000')
+time.sleep(5)
+driver.find_element_by_xpath('//*[@id="addCartBtn"]/span').click()
+
+time.sleep(5)
+mevcutStok = driver.find_element_by_id('myPopup3').text
+
+print(mevcutStok.split(': ')[1])
+
+driver.close()
+
+driver = webdriver.Chrome('C:/KomutSatiriProg/chromedriver.exe')  # Optional argument, if not specified will search path.
+driver.set_window_position(0,0)
+driver.set_window_size(800,800)
+driver.get('https://www.robotistan.com/makeblock-mbot-ranger-bluetooth')
+time.sleep(5)
+driver.find_element_by_name('Adet5798').send_keys('100000')
+time.sleep(5)
+driver.find_element_by_xpath('//*[@id="addCartBtn"]/span').click()
+
+time.sleep(5)
+mevcutStok = driver.find_element_by_id('myPopup3').text
+
+print(mevcutStok.split(': ')[1])
+
+driver.close()
