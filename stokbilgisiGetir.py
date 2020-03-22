866e899520506b9a1a70136dcbf9bbf306b67464
civanahmetyasin <civan.ahmetyasin@gmail.com>
2020-03-22 14:18:24 +0300

    Zaman degerleri yukseltidi yoksa butona yetisemiyor.

--- stokbilgisiGetir.py
+++ stokbilgisiGetir.py
@@ -14,12 +14,12 @@ driver.set_window_size(800,800)
 while True:
     driver.get(urunURL)
     urunAdi = driver.find_element_by_id('productName').text
-    time.sleep(2)
+    time.sleep(5)
     driver.find_element_by_name(adetYaziKutusu).send_keys('1000000000')
-    time.sleep(2)
+    time.sleep(5)
     driver.find_element_by_xpath('//*[@id="addCartBtn"]/span').click()
     
-    time.sleep(2)
+    time.sleep(5)
     mevcutStok = driver.find_element_by_id('myPopup3').text
     
     print( urunAdi + " : " + mevcutStok.split(': ')[1])
