397913b63aba1334ad6d0f40a2e551a31d208351
civanahmetyasin <civan.ahmetyasin@gmail.com>
2020-03-22 14:13:57 +0300

    bir seferlige mahsus sayfa ekrana getiriliyor.

--- stokbilgisiGetir.py
+++ stokbilgisiGetir.py
@@ -4,14 +4,18 @@ import time
 urunURL = 'https://www.robotistan.com/makeblock-mbot-bluetooth-kiti-v11-mavi'
 adetYaziKutusu = 'Adet2754'
 
+urunURL = 'https://www.robotistan.com/arduino-uno-r3-klon-usb-kablo-hediyeli-usb-chip-ch340'
+adetYaziKutusu = 'Adet2729'
+
 driver = webdriver.Chrome('chromedriver.exe')  # Optional argument, if not specified will search path.
+driver.set_window_position(0,0)
+driver.set_window_size(800,800)
+
 while True:
-    #driver.set_window_position(0,0)
-    #driver.set_window_size(800,800)
     driver.get(urunURL)
     urunAdi = driver.find_element_by_id('productName').text
     time.sleep(2)
-    driver.find_element_by_name(adetYaziKutusu).send_keys('100000')
+    driver.find_element_by_name(adetYaziKutusu).send_keys('1000000000')
     time.sleep(2)
     driver.find_element_by_xpath('//*[@id="addCartBtn"]/span').click()
     
