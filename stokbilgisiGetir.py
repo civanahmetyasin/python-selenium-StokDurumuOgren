5958d32fd3136ca39120cc24bf179c1d5957de58
civanahmetyasin <civan.ahmetyasin@gmail.com>
2020-03-22 13:57:58 +0300

    Sonsuz dongu ile surekli olarak verileri alacak sekilde hazilandi diger urun icim yapilan calisma yorum satirana alindi.

--- stokbilgisiGetir.py
+++ stokbilgisiGetir.py
@@ -1,29 +1,33 @@
 from selenium import webdriver
 import time
 
-driver = webdriver.Chrome('chromedriver.exe')  # Optional argument, if not specified will search path.
-driver.set_window_position(0,0)
-driver.set_window_size(800,800)
-driver.get('https://www.robotistan.com/makeblock-mbot-bluetooth-kiti-v11-mavi')
-time.sleep(5)
-driver.find_element_by_name('Adet2754').send_keys('100000')
-time.sleep(5)
-driver.find_element_by_xpath('//*[@id="addCartBtn"]/span').click()
+while True:
+    driver = webdriver.Chrome('chromedriver.exe')  # Optional argument, if not specified will search path.
+    driver.set_window_position(0,0)
+    driver.set_window_size(800,800)
+    driver.get('https://www.robotistan.com/makeblock-mbot-bluetooth-kiti-v11-mavi')
+    urunAdi = driver.find_element_by_id('productName').text
+    time.sleep(2)
+    driver.find_element_by_name('Adet2754').send_keys('100000')
+    time.sleep(2)
+    driver.find_element_by_xpath('//*[@id="addCartBtn"]/span').click()
+    
+    time.sleep(2)
+    mevcutStok = driver.find_element_by_id('myPopup3').text
+    
+    print( urunAdi + " : " + mevcutStok.split(': ')[1])
+    time.sleep(10)
 
-time.sleep(5)
-mevcutStok = driver.find_element_by_id('myPopup3').text
-
-print(mevcutStok.split(': ')[1])
-
-driver.get('https://www.robotistan.com/makeblock-mbot-ranger-bluetooth')
-time.sleep(5)
-driver.find_element_by_name('Adet5798').send_keys('100000')
-time.sleep(5)
-driver.find_element_by_xpath('//*[@id="addCartBtn"]/span').click()
-
-time.sleep(5)
-mevcutStok = driver.find_element_by_id('myPopup3').text
-
-print(mevcutStok.split(': ')[1])
-
-driver.close()
+#driver.get('https://www.robotistan.com/makeblock-mbot-ranger-bluetooth')
+#urunAdi = driver.find_element_by_id('productName').text
+#time.sleep(2)
+#driver.find_element_by_name('Adet5798').send_keys('100000')
+#time.sleep(2)
+#driver.find_element_by_xpath('//*[@id="addCartBtn"]/span').click()
+#
+#time.sleep(2)
+#mevcutStok = driver.find_element_by_id('myPopup3').text
+#
+#print( urunAdi + " : " + mevcutStok.split(': ')[1])
+#
+#driver.close()
