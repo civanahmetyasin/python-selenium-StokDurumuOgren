a476ef98573b80cfa341eb07ed45fa2e4fb69f80
civanahmetyasin <civan.ahmetyasin@gmail.com>
2020-03-22 17:35:47 +0300

    Stok bilgileri bir txt dosyasina ekleniyor.

--- stokbilgisiGetir.py
+++ stokbilgisiGetir.py
@@ -1,8 +1,13 @@
 from selenium import webdriver
 import time
+import datetime
+
+tarihSaat = datetime.datetime.now()
+ORNEKLEME_SURESI_SANIYE = 10
+
+
 
 print("Urun Linki Giriniz")
-
 urunURL = raw_input()
 print(urunURL)
 
@@ -10,14 +15,13 @@ print("Yazi Kutusu ID Giriziniz - Adet2729 gibi")
 adetYaziKutusu = raw_input()
 print(adetYaziKutusu)
 
-#urunURL = 'https://www.robotistan.com/arduino-uno-r3-klon-usb-kablo-hediyeli-usb-chip-ch340'
-#adetYaziKutusu = 'Adet2729'
 
 driver = webdriver.Chrome('chromedriver.exe')  # Optional argument, if not specified will search path.
 driver.set_window_position(0,0)
 driver.set_window_size(800,800)
 
 while True:
+    logDosyasi = open("stokBilgileri.txt","a")
     driver.get(urunURL)
     urunAdi = driver.find_element_by_id('productName').text
     time.sleep(5)
@@ -29,6 +33,12 @@ while True:
     mevcutStok = driver.find_element_by_id('myPopup3').text
     
     print( urunAdi + " : " + mevcutStok.split(': ')[1])
-    time.sleep(10)
+
+    logDosyasi.write("%d" % tarihSaat.year + "-" +"%d" % tarihSaat.month + "-" + "%d" % tarihSaat.day +"-"+ "%d" % tarihSaat.hour + "-" + "%d" % tarihSaat.minute)    
+    logDosyasi.write(" : " + mevcutStok.split(': ')[1] + "\n")
+    logDosyasi.close()
+    time.sleep(ORNEKLEME_SURESI_SANIYE)
+
+
 
 driver.close()
